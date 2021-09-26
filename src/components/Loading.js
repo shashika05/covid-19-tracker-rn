380d11e3961ec160de1384f1b427e4b0dcf890b6
shashika05 <shashikaraveenyasas@gmail.com>
2021-09-26 19:18:32 +0530

    Changed Loading compenent into Function compenent

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,20 +3,18 @@ import { View, Text } from "react-native";
 import LottieView from "lottie-react-native";
 import tailwind from "tailwind-rn";
 
-export default class Loading extends React.Component {
-  render() {
-    return (
-      <View style={tailwind("justify-center items-center")}>
-        <LottieView
-          style={tailwind("w-52 h-52")}
-          source={require("../../assets/loading2.json")}
-          autoPlay
-          loop
-        />
-        <Text style={{ fontFamily: "PoppinsLight", fontSize: 16 }}>
-          Loading..
-        </Text>
-      </View>
-    );
-  }
+export default function Loading() {
+  return (
+    <View style={tailwind("justify-center items-center")}>
+      <LottieView
+        style={tailwind("w-52 h-52")}
+        source={require("../../assets/loading2.json")}
+        autoPlay
+        loop
+      />
+      <Text style={{ fontFamily: "PoppinsLight", fontSize: 16 }}>
+        Loading..
+      </Text>
+    </View>
+  );
 }
